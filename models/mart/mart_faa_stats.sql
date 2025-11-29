22ec5f48236f35b53a2e96353fc8e60e373b9564
Teresa Mesia Lozano <teresamesialozano@MacBook-Pro-von-Teresa.local>
2025-11-29 17:01:52 +0100

    change

--- models/mart/mart_faa_stats.sql
+++ models/mart/mart_faa_stats.sql
@@ -1,18 +1,4 @@
-{{
-  config(
-    materialized='joined_table'
-  )
-}}
-SELECT
-    a.faa,
-    a.name AS airport_name,
-    a.city,
-    a.country,
-    f.*
-FROM
-    {{ ref('prep_airports') }} a
-LEFT JOIN
-    {{ ref('prep_flights') }} f ON a.faa = f.origin
+
 
 
 WITH stats AS (
