47a925ca06c9fc047dac7442f902df2521f94b37
Teresa Mesia Lozano <teresamesialozano@MacBook-Pro-von-Teresa.local>
2025-12-01 10:14:14 +0100

    change

--- models/mart/mart_route_stats.sql
+++ models/mart/mart_route_stats.sql
@@ -3,33 +3,37 @@
 
 WITH origin_info AS (
     SELECT DISTINCT ON (origin) origin, city, country, airport_name
-    FROM joined_table
+    FROM {{ ref('joined_table') }}
 ),
 dest_info AS (
     SELECT DISTINCT ON (dest) dest, city, country, airport_name
-    FROM joined_table
+    FROM {{ ref('joined_table') }}
 )
-select
-    o.origin as origin_airport,
-    o.dest as dest_airport,
-    COUNT(*) as total_flights_this_route,
-    SUM(CASE WHEN o.cancelled = 1 THEN 1 ELSE 0 END) as total_flights_cancelled,
-    SUM(CASE WHEN o.diverted = 1 THEN 1 ELSE 0 END) as total_flights_diverted,
-    COUNT(DISTINCT tail_number) as unique_airplanes,
-    COUNT(DISTINCT "airline") as unique_airlines,
-    AVG(o.actual_elapsed_time) as avg_el_time,
-    AVG(o.arr_delay) as avg_arr_delay,
-    MAX(o.arr_delay) as max_arr_delay,
-    MIN(o.arr_delay) as min_arr_delay,
-    oi.city as origin_city,
-    oi.country as origin_country,
-    oi.airport_name as origin_airport_name,
-    di.city as dest_city,
-    di.country as dest_country,
-    di.airport_name as dest_airport_name
-from {{ ref('joined_table') }}
-left join  origin_info oi on o.origin = oi.origin
-left join dest_info di on o.dest = di.dest
-group by o.origin, o.dest, oi.city, oi.country, oi.airport_name, di.city, di.country, di.airport_name
-order by o.origin, o.dest
+SELECT
+    o.origin AS origin_airport,
+    o.dest AS dest_airport,
+    COUNT(*) AS total_flights_this_route,
+    SUM(CASE WHEN o.cancelled = 1 THEN 1 ELSE 0 END) AS total_flights_cancelled,
+    SUM(CASE WHEN o.diverted = 1 THEN 1 ELSE 0 END) AS total_flights_diverted,
+    COUNT(DISTINCT o."tail_number") AS unique_airplanes,
+    COUNT(DISTINCT o."airline") AS unique_airlines,
+    AVG(o.actual_elapsed_time) AS avg_el_time,
+    AVG(o.arr_delay) AS avg_arr_delay,
+    MAX(o.arr_delay) AS max_arr_delay,
+    MIN(o.arr_delay) AS min_arr_delay,
+    oi.city AS origin_city,
+    oi.country AS origin_country,
+    oi.airport_name AS origin_airport_name,
+    di.city AS dest_city,
+    di.country AS dest_country,
+    di.airport_name AS dest_airport_name
+FROM
+    {{ ref('joined_table') }} o
+LEFT JOIN origin_info oi ON o.origin = oi.origin
+LEFT JOIN dest_info di ON o.dest = di.dest
+GROUP BY
+    o.origin, o.dest, oi.city, oi.country, oi.airport_name, di.city, di.country, di.airport_name
+ORDER BY
+    o.origin, o.dest
+
 
